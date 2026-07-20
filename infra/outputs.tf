fb5893525a794ee766785d57b1b17374ed3171ad
Jairo Júnior <jairojjr2003@gmail.com>
2026-07-20 19:45:12 -0300

    fix: cloudfront_distribution_id not found

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,7 @@ output "frontend_url" {
   value = "https://${aws_cloudfront_distribution.frontend.domain_name}"
 
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
